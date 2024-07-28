5e2912dff286f93a8a16752eb12a6d51a94fd7ff
OctavioOP <oc.opazo@outlook.com>
2024-07-28 19:22:58 -0400

    permisos a traves del cors

--- TIMS/settings.py
+++ TIMS/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #https://github.com/adamchainz/django-cors-headers
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
